4413e6c7228f8fa7164e818b84295aded7da9f6e
Oussama Seqqam <ouseqqam@e2r4p9.1337.ma>
2022-06-02 18:56:24 +0100

    edit

--- computerv1.py
+++ computerv1.py
@@ -1,10 +1,16 @@
 import re
+import sys
 
 def sqrt(nb):
     return nb ** 0.5
 
+
+
+if len(sys.argv) != 2:
+    exit("ERROR")
+
+st = sys.argv[1]
 regex = '^(([-+]\s?)?\d*(\.?\d+)?\s?\*\s?(X\s?(\^\s?[+]?\d+))\s?)+=\s?(0|((([-+]\s)?\d*(\.?\d+)?\s?\*\s?(X\s?(\^\s?[+]?\d+))\s?)+))'
-st = "-33 * X ^ 2 + 5 * X ^ 1 + 1 * X ^ 0 = 0"
 x = re.search(regex, st)
 
 if x == None:
@@ -85,26 +91,28 @@ elif degree == 2:
     b = data[1]['coef']
     c = data[2]['coef']
 
-    delta = b * b - 4 * a * c
+    delta = b * b - (4 * a * c)
 
     if delta == 0:
-        x0 = -b / 2 * a
+        x0 = -b / (2 * a)
         print("x0 = ", x0)
     elif delta > 0 :
-        x1 = -b - sqrt(delta) / 2 * a
-        x2 = -b + sqrt(delta) / 2 * a
+        x1 = (-b - sqrt(delta)) / (2 * a)
+        x2 = (-b + sqrt(delta)) / (2 * a)
         print("Discriminant is strictly positive, the two solutions are:")
         print( x1)
         print(x2)
     if delta < 0:
         delta = -delta
-        z1r = -b / 2 * a
-        z1i = sqrt(delta) / 2 * a
+        z1r = -b / (2 * a)
+        z1i = sqrt(delta) / (2 * a)
         print("Discriminant is strictly negative, the equation have two solution in C:")
         print(str(z1r) + " - i * " + str(z1i))
         print(str(z1r) + " + i * " + str(z1i))
 elif degree == 1:
     x = -1 * data[1]['coef'] /  data[0]['coef']
+    print("The solution is:")
+    print(x)
 elif degree == 0 and len(data) == 1:
     if data[0]['coef'] != 0:
         print("The equation has no solution.")
